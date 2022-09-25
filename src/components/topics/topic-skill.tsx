f57b288500c791fb9b9930f4c3fc1000afc3a523
ITO Keita <kk79it@gmail.com>
2022-09-25 11:35:31 +0900

    Skillの変更

--- src/components/topics/topic-skill.tsx
+++ src/components/topics/topic-skill.tsx
@@ -1,3 +1,5 @@
+import styles from "../../styles/skill.module.scss";
+
 function Articles() {
   return (
     <div className="flex min-h-screen items-center justify-center">
@@ -5,14 +7,45 @@ function Articles() {
         <h2 className="text-4xl font-bold tracking-wider text-gray-700">
           Skill
         </h2>
-        <div>
-          <h3>言語について</h3>
-          <ul>
-            <li>Java</li>
-            <li>Node.js</li>
-            <li>JavaScript（Web）</li>
-            <li>C#（少しだけ）</li>
-          </ul>
+        <div className={styles.skill}>
+          <div>
+            <h3>Web制作</h3>
+            <div>
+              <h4>Next.js, React（TypeScript）</h4>
+              <p>
+                高校の文化祭のサイトなどをNext.jsで作成しました
+                <br />
+                他にも簡単なツールなどをReactで作成
+                <br />
+                Firebaseと連携してリアルタイムで交流できるゲームも作りました
+              </p>
+            </div>
+            <div>
+              <h4>Vue.js, Google Apps Script（TypeScript）</h4>
+              <p>学校で利用する健康観察フォームを作成しました</p>
+            </div>
+          </div>
+          <div>
+            <h3>デスクトップアプリ制作</h3>
+            <div>
+              <h4>Swing（Java）</h4>
+              <p>ファイルコピーの補助ソフトを作成しました</p>
+            </div>
+            <div>
+              <h4>Electron（TypeScript）</h4>
+            </div>
+          </div>
+          <div>
+            <h3>その他</h3>
+            <div>
+              <h4>C#</h4>
+              <p>WPFでデスクトップアプリを作成</p>
+            </div>
+            <div>
+              <h4>Ruby, C</h4>
+              <p>アプリ等の作成はありませんが、基本的な文法は</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
