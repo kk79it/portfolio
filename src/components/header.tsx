f3dc6911a69ca19232689ba0e35c44e7f4d6fbca
ITO Keita <kk79it@gmail.com>
2022-09-25 16:58:04 +0900

    表示位置を追跡できるように

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,52 @@
+import classNames from "classnames";
 import { useReadingTopic } from "../hooks/reading-topic";
+import HashLink from "./hash-link";
+
+const items = [
+  {
+    name: "Home",
+    path: "home",
+  },
+  {
+    name: "About",
+    path: "about",
+  },
+  {
+    name: "Skill",
+    path: "skill",
+  },
+];
 
 function Header() {
   const [readingTopic] = useReadingTopic();
 
   return (
-    <div className="bg-red-400 flex h-16 items-center">{readingTopic}</div>
+    <div className="bg-red-400">
+      <div className="mx-auto flex h-16 max-w-4xl items-center">
+        <div className="text-2xl font-bold tracking-wider text-gray-700">
+          K-Lab.
+        </div>
+        <div className="ml-auto">
+          <ul className="flex">
+            {items.map(({ name, path }) => (
+              <li key={path} className="ml-3">
+                <HashLink
+                  className={classNames(
+                    "font-bold uppercase tracking-wider text-gray-700",
+                    {
+                      "border-b-2 border-gray-700": path === readingTopic,
+                    }
+                  )}
+                  to={path}
+                >
+                  {name}
+                </HashLink>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
   );
 }
 
