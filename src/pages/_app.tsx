c5c3f115aa3c57ac9fcb6aee1e3f53944a3937be
ITO Keita <kk79it@gmail.com>
2022-10-02 00:19:34 +0900

    Google Analyticsの導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,31 @@
 import type { AppProps } from "next/app";
+import Script from "next/script";
+import { usePageView } from "../hooks/page-view";
 import ReadingTopicProvider from "../hooks/reading-topic";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  usePageView();
+
   return (
-    <ReadingTopicProvider>
-      <Component {...pageProps} />
-    </ReadingTopicProvider>
+    <>
+      <ReadingTopicProvider>
+        <Component {...pageProps} />
+      </ReadingTopicProvider>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-4GD0SP0XQ7"
+      />
+      <Script id="gtag">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          
+          gtag('config', 'G-4GD0SP0XQ7');
+          `}
+      </Script>
+    </>
   );
 }
 
